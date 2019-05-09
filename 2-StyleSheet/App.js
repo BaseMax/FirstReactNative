00be1917d4e95a0bbb689ce70c4fa9d37b77bcb9
Max Base <MaxBaseCode@Gmail.Com>
2019-05-09 23:44:52 +0430

    Add files via upload

---
+++ 2-StyleSheet/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import MyAlert from './alert.js'
+import { Text, StyleSheet } from 'react-native'
+
+const App = () => {
+	return (
+		<Text style = {styles.text}>Hello, World!</Text>
+	)
+}
+export default App
+
+const styles = StyleSheet.create ({
+	text: {
+		backgroundColor: '#4ba37b',
+		width: '100%',
+		textAlign: 'center',
+		borderRadius: 50,
+		alignItems: 'center',
+		marginTop: 100,
+		padding: 25,
+		fontSize: 50,
+		marginBottom: 100,
+	}
+})
