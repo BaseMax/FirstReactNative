6c2c4f1971875c929eb02a21a3dea71ba8cb4896
Max Base <MaxBaseCode@Gmail.Com>
2019-05-10 00:37:46 +0430

    Update App.js

--- 5-UpDownNumberButton/App.js
+++ 5-UpDownNumberButton/App.js
@@ -1,39 +1,73 @@
-/**
- * Sample React Native App
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-type Props = {};
-export default class App extends Component<Props> {
+import React, { Component } from 'react'
+import {
+	StyleSheet,
+	TouchableHighlight,
+	Text,
+	View,
+} from 'react-native'
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { count: 0 }
+	}
+	onPressUp = () => {
+		this.setState({
+			count: this.state.count + 1
+		})
+	}
+	onPressDown = () => {
+		this.setState({
+			count: this.state.count - 1
+		})
+	}
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to App!</Text>
-				<Text style={styles.instructions}>Hey My friend!</Text>
+			<View>
+				<TouchableHighlight onPress={this.onPressUp}>
+					<Text style={styles.buttonUp}> UP </Text>
+				</TouchableHighlight>
+				<TouchableHighlight onPress={this.onPressDown}>
+					<Text style={styles.buttonDown}> DOWN </Text>
+				</TouchableHighlight>
+				<View style={[styles.countContainer]}>
+					<Text style={[styles.countText]}>
+						{ this.state.count !== 0 ? this.state.count: null}
+					</Text>
+				</View>
 			</View>
-		);
+		)
 	}
 }
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
+	buttonUp: {
+		backgroundColor: 'green',
+		width: '100%',
+		textAlign: 'center',
+		borderRadius: 50,
 		alignItems: 'center',
-		backgroundColor: 'yellow',
+		marginTop: 100,
+		padding: 15,
+		fontSize: 50,
+		marginBottom: 100,
 	},
-	welcome: {
-		fontSize: 38,
+	buttonDown: {
+		backgroundColor: 'red',
+		width: '100%',
 		textAlign: 'center',
-		margin: 20,
+		borderRadius: 50,
+		alignItems: 'center',
+		marginTop: 100,
+		padding: 15,
+		fontSize: 50,
+		marginBottom: 100,
 	},
-	instructions: {
-		fontSize: 25,
-		textAlign: 'center',
-		color: 'blue',
-		marginTop: 10,
+	countContainer: {
+		alignItems: 'center',
+		padding: 10
 	},
-});
+	countText: {
+		color: '#FF00FF'
+	}
+})
 
+export default App
